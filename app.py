fd3051a4e0c6ae2ceafc802960a2003932438da6
onlydurodola <nowonlywork@gmail.com>
2025-02-03 13:24:36 +0100

    running locally

--- app.py
+++ app.py
@@ -32,17 +32,17 @@ def get_fun_fact(n):
 @app.route('/api/classify-number', methods=['GET'])
 def classify_number():
     number = request.args.get('number')
-    
+
     if not number.isdigit():
         return jsonify({"number": number, "error": True}), 400
-    
+
     num = int(number)
     properties = []
-    
+
     if is_armstrong(num):
         properties.append("armstrong")
     properties.append("odd" if num % 2 != 0 else "even")
-    
+
     response_data = {
         "number": num,
         "is_prime": is_prime(num),
@@ -51,8 +51,8 @@ def classify_number():
         "digit_sum": sum(int(d) for d in str(num)),
         "fun_fact": get_fun_fact(num)
     }
-    
+
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=5000, debug=True)
