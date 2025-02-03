06a4d8e4cffad1cea8b9b6d89e346e8cdca078f1
onlydurodola <nowonlywork@gmail.com>
2025-02-03 09:07:26 +0100

    fixed app.py

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(_name_)
+
+# CORS
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+# Functions
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def is_perfect(n):
+    if n < 2:
+        return False
+    divisors = [i for i in range(1, n) if n % i == 0]
+    return sum(divisors) == n
+
+def is_armstrong(n):
+    digits = [int(d) for d in str(n)]
+    length = len(digits)
+    return sum(d**length for d in digits) == n
+
+def digit_sum(n):
+    return sum(int(d) for d in str(n))
+
+def get_fun_fact(n):
+    url = f"http://numbersapi.com/{n}/math"
+    response = requests.get(url)
+    return response.text  # Directly return the API response text
+
+# Main endpoint
+@app.route('/api/classify-number', methods=['GET'])
+def classify_number():
+    number = request.args.get('number')
+    
+    # Input validation
+    if not number or not number.lstrip('-').isdigit():
+        return jsonify({
+            "number": number if number else "null",
+            "error": True
+        }), 400
+    
+    number = int(number)
+    
+    # Determine properties
+    properties = []
+    if is_armstrong(number):
+        properties.append("armstrong")
+    if number % 2 == 0:
+        properties.append("even")
+    else:
+        properties.append("odd")
+    
+   
+    response = {
+        "number": number,
+        "is_prime": is_prime(number),
+        "is_perfect": is_perfect(number),
+        "properties": properties,
+        "digit_sum": digit_sum(number),
+        "fun_fact": get_fun_fact(number)  
+    }
+    
+    return jsonify(response), 200
+
+if _name_ == '_main_':
+    app.run(debug=True)
