bf10e19f5e90295e3b5e5df3a5975aa1821915a7
onlydurodola <nowonlywork@gmail.com>
2025-02-03 10:05:13 +0100

    modified

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -74,5 +75,5 @@ def classify_number():
     
     return jsonify(response), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
